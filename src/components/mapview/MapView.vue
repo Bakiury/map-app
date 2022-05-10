cc3aa2052c9ce8d2cdff0ea35b7cc9af31179d34
Bakiury <thecutaseta@gmail.com>
2022-05-10 12:04:28 -0500

    Preparing the space to place the map

---
+++ src/components/mapview/MapView.vue
@@ -0,0 +1,14 @@
+<script lang="ts" src="./MapView.ts"></script>
+
+<template>
+    <h1>MapView</h1>
+    <p>
+        {{ isLoading }}
+        {{ userLocation }}
+        {{ isUserLocationReady }}
+    </p>
+</template>
+
+<style scoped>
+
+</style>
