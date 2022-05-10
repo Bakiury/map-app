b9e13e3613bcf08f3e793f575bac1cc3c101c3f2
Bakiury <thecutaseta@gmail.com>
2022-05-10 14:56:36 -0500

    Mapbox implemented as soon as we have the user's location

--- src/components/mapview/MapView.vue
+++ src/components/mapview/MapView.vue
@@ -1,14 +1,33 @@
 <script lang="ts" src="./MapView.ts"></script>
 
 <template>
-    <h1>MapView</h1>
-    <p>
-        {{ isLoading }}
-        {{ userLocation }}
-        {{ isUserLocationReady }}
-    </p>
+    <div v-if="!isUserLocationReady" class="lodaing-map d-flex justify-content-center align-items-center">
+        <div class="text-center">
+            <h3>Espere por favor</h3>
+            <span>Localizando...</span>
+        </div>
+    </div>
+
+    <div v-show="isUserLocationReady" class="map-container" ref="mapElement">
+        
+    </div>
 </template>
 
 <style scoped>
+.lodaing-map {
+    background-color: rgba(0, 0, 0, 0.8);
+    color: #ffffff;
+    height: 100vh;
+    left: 0px;
+    position: fixed;
+    top: 0px;
+    width: 100vw;
+    z-index: 9999;
+}
 
+.map-container {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmFraXVyeSIsImEiOiJjbDMwaW03MzkxcDZsM2ZwMmZnemNzNzI4In0.U4xGtcGL6tB4an3ShYfGpw';
+
 if (!navigator.geolocation) {
     throw new Error('Tu navegador no soporta el Geolocation');
 }
